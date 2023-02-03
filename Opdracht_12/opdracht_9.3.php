fe48e311cc8392518e09627c3bd93102e94d899f
MaohuaFan <9019910@student.zadkine.nl>
2023-02-03 13:33:58 +0100

    php

---
+++ Opdracht_12/opdracht_9.3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_9.3.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        try {
+        $db = new PDO("mysql:host=localhost;dbname=cijfersysteem", "root", "");
+        $query = $db->prepare("SELECT * FROM cijfersysteem");
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        echo"<table>";
+            foreach($result as &$data) {
+                echo "<tr>";
+                echo "<td>" . $data["leerling"] . "</td>";
+                echo "<td>" . $data["cijfer"] . "</td>";
+            echo "</tr>";
+        echo"<table>";
+        }
+        }   catch(PDOException $e) {
+            die("Error!: " . $e->getMessage());
+        }
+    ?>
+</body>
+</html>

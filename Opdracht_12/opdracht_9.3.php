1efb8c2c5d3314d226cdcd898f969139e471f24b
MaohuaFan <9019910@student.zadkine.nl>
2023-02-03 13:45:57 +0100

    php

--- Opdracht_12/opdracht_9.3.php
+++ Opdracht_12/opdracht_9.3.php
@@ -15,13 +15,19 @@
         $query->execute();
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
         echo"<table>";
+            echo "<tr>";
+                echo "<td>" . "Leerling ID" . "</td>";
+                echo "<td>" . "Leerling Naam" . "</td>";
+                echo "<td>" . "Cijfer" . "</td>";
+            echo "</tr>";
             foreach($result as &$data) {
                 echo "<tr>";
-                echo "<td>" . $data["leerling"] . "</td>";
-                echo "<td>" . $data["cijfer"] . "</td>";
-            echo "</tr>";
+                    echo "<td>" . $data["id"] . "</td>";
+                    echo "<td>" . $data["leerling"] . "</td>";
+                    echo "<td>" . $data["cijfer"] . "</td>";
+                echo "</tr>";
+            }
         echo"<table>";
-        }
         }   catch(PDOException $e) {
             die("Error!: " . $e->getMessage());
         }

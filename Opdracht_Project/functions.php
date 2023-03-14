f34824c48b3c8bf4f33fe402b23e5e577530bbed
MaohuaFan <9019910@student.zadkine.nl>
2023-03-14 10:02:10 +0100

    php

--- Opdracht_Project/functions.php
+++ Opdracht_Project/functions.php
@@ -1,3 +1,8 @@
 <?php
-
+    function showRadio(){
+        $radioVal = $("input[name='1']:checked").val();
+        if(radioVal) {
+            $( "#radioMsg" ).html("<p>"+radioVal+"</p>");
+        }
+    }
 ?>
--- Opdracht_Project/main.php
+++ Opdracht_Project/main.php
@@ -1,24 +1,16 @@
-<!DOCTYPE html>
-<html lang="en"><head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head><body>
-    <h3>Lakkleur</h3>
+    <?php
+    
+        if(!empty($_GET['radio'])){ $selected = $_GET['radio'];}
+        else{ $selected = '1';}
+    ?>
     <form action="#" method="post">
         <label for="1">Color 1: <input type="radio" name="radio" value="1" id="1" checked></label>
         <label for="2">Color 2: <input type="radio" name="radio" value="2" id="2"></label>
-        <label for="3">Color 3: <input type="radio" name="radio" value="3" id="3"></label>
+        <label for="3">Color 3: <input type="radio" name="radio" value="3" id="3"></label><br>
+        <input type="radio" name="a1" value="site2" onchange="showRadio()" /> Site 2 <?php echo ($selected == 'zbc' ? 'This was selected!' : '');?> </br>
+        <input type="submit" name="submit" value="submit">
         </form><br>
     <?php
-    /*
-        echo'<form action="" method="post">';
-        echo'<label for=""></label>';
-        echo'<input type="radio" name="radio" id="1" checked>';
-        echo'<input type="submit" value="">';
-        echo'</form>';
-    */
     echo"Echo: ";
     $radio = $_POST['radio'];
         if(isset($_POST['submit'])){
@@ -35,5 +27,10 @@
             }
         }
     ?>
-
-</body></html>
+<span class="r-text"><?php echo $selected;?></span>
+<script>
+    $('input[type=radio]').click(function(e) {//jQuery works on clicking radio box
+        var value = $(this).val(); //Get the clicked checkbox value
+        $('.r-text').html(value);
+    });
+</script>
